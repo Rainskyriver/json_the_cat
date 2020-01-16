91c189cc1ac2ce2b3e40447e1c6fb8a190527c46
Lian Yelich <lianyelich14@gmail.com>
2020-01-16 22:29:27 +0000

    Installed mocha and chai, added a test directory, and a BreedFetcherTest.js file

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,17 +4,16 @@ const request = require('request');
 const fetchBreedDescription = function(breedName, callback) {
   request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, 'utf8', (error, response, body) => {
     if (error) {
-      callback(error);
+      callback(error, null);
     }
   
     const data = JSON.parse(body);
   
     if (data[0] === undefined) {
-      return console.log('Invalid cat name, please try again');
-      
+      callback('Invalid cat breed, please try a valid breed');
     }
 
-    callback(data[0].description);
+    callback(null, data[0].description);
   });
 };
 
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const breedName = process.argv[2];
+// index.js
 const { fetchBreedDescription } = require('./breedFetcher');
 
-fetchBreedDescription(breedName, (desc, error) => {
+const breedName = process.argv[2];
+
+fetchBreedDescription(breedName, (error, desc) => {
   if (error) {
-    console.log('Error fetch details: ',  error);
+    console.log('Error fetch details:', error);
   } else {
     console.log(desc);
   }
