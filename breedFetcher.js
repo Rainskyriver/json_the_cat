d01745fe767783e52d146781e188bc1667b4c93e
Lian Yelich <lianyelich14@gmail.com>
2020-01-16 21:45:29 +0000

    Refactored breedFetcher.js and added index.js to make my code more modular and to test it more easily in the future

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,21 @@
 const request = require('request');
-const args = process.argv.slice(2);
 
-request('https://api.thecatapi.com/v1/breeds/search?q=' + args[0], 'utf8', (err, response, body) => {
-  if (err) {
-    return console.log('Encountered an error: ', err);
-  }
 
-  const data = JSON.parse(body);
-
-  if (data[0] === undefined) {
-    return console.log('Invalid cat name, please try again');
-    
-  }
+const fetchBreedDescription = function(breedName, callback) {
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, 'utf8', (error, response, body) => {
+    if (error) {
+      callback(error);
+    }
   
-  console.log(data[0].description);
-});
+    const data = JSON.parse(body);
+  
+    if (data[0] === undefined) {
+      return console.log('Invalid cat name, please try again');
+      
+    }
+
+    callback(data[0].description);
+  });
+};
+
+module.exports = { fetchBreedDescription };
